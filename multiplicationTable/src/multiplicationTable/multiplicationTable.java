aff37247e3ed8c59391a0d4033fb184801d3d724
kevinjennings <kevinjennings@10.0.0.24>
2020-12-14 14:08:38 -0500

    commit for mult table

---
+++ multiplicationTable/src/multiplicationTable/multiplicationTable.java
@@ -0,0 +1,16 @@
+package multiplicationTable;
+
+public class multiplicationTable {
+	
+	public static void main(String[] args) {
+	   for (int i =1; i <= 10; i++){
+	        //cycles through the first number to multiply
+
+	        for (int b=1; b <=10; b++){
+	            //cycles through second number to multiply
+	            System.out.print(i*b + " ");
+		        }
+		        System.out.println();
+		    }
+		}
+}
